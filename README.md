098042bb02e332ce04fd240e8309471871ce7e1d
Jeremiah Bailey <jeremytrd@gmail.com>
2023-06-05 11:29:48 -0600

    Publishing http CF version
    This version provides a central http cloud function to manage service account keys across an org and removes the need for implementing locksmith wihtin multiple applications.

--- README.md
+++ README.md
@@ -1,66 +1,21 @@
 # locksmith
- Locksmith allows you to create GCP Service Account keys, vault them in secret manager, rotate the keys and secrets, and disable both keys and versions.
+Locksmith seeks to solve a common problem in GCP organizations in which integrations with 3dP applications/services require a GCP service account key (Example services: [tenable](https://docs.tenable.com/cloud-security/Content/QuickReference/OnboardGCPServiceAccount.htm), [trend micro](https://cloudone.trendmicro.com/docs/workload-security/gcp-account-create/), [prisma](https://docs.paloaltonetworks.com/prisma/prisma-cloud/prisma-cloud-admin/connect-your-cloud-platform-to-prisma-cloud/onboard-gcp/onboard-gcp-project)). Because this is a common pattern in GCP and service account keys present a risk due to their long-lived credentials, organizations are forced to make a tradeoff between rejecting a solution which may provide a genuine security service/improvement to their posture or rejecting the solution because of its reliance on service account keys. Because this is a common enough pattern in GCP there's a reasonable need to offer a stable, repeatable solution for addressing this tradeoff across an enterprise/GCP org.
 
+A custom service such as locksmith removes the need to grant service account key permissions to developers and instead ensures that service account keys created in a given GCP Org are managed in a consistent manner.
 
-# Example
+Locksmith simplifies the management of service account keys in a few key ways:
+1. It orchestrates creating, vaulting (placing in GCP Secrets Manager), rotating, and disabling of keys.
+2. Because the management of the keys is eased, it becomes a low cost effort to frequently rotate the service account keys,
+which reduces the primary issue with service account keys (the long-lived nature of the credential).
+1. It provides a natural funnel/forcing mechanism to ensure all keys in a given org are managed consistently. Additionally, configurations
+could be established to ensure keys are rotated within a certain time frame while easily notifying developer teams/applications that a new
+version has been vaulted in secrets manager.
+1. [Asset Key Thief](https://engineering.sada.com/asset-key-thief-disclosure-cfae4f1778b6) provides a clear example of how a misconfiguration
+within other GCP services can force organizations to need to rapidly rotate all or some keys within their GCP environment. Locksmith can address
+this situation easily with the emergency key rotation option.
 
-## Background Cloud Function
-This example assumes you want to use locksmith within a background Cloud Function that will receive messages from PubSub. The message published to the topic in this instance would
-be a 'Directive' for locksmith in JSON. See example directive below.
-```go
-package cloudfunctionexample
+## Deployment Configuration
 
-import (
-	"context"
-	"encoding/json"
-	"log"
+The Service Account used to run the cloud function will require the following permissions: 
 
-	"github.com/jeremiahbailey/locksmith"
-)
-
-type PubSubMessage struct {
-	Data []byte `json:"data"`
-}
-
-// Unmarshals the PubSub message into the Directive.
-func unmarshalMessage(m PubSubMessage) locksmith.Directive {
-	log.Println("Starting to unmarshal the message...")
-	var d locksmith.Directive
-	json.Unmarshal(m.Data, &d)
-
-	return d
-
-}
-
-func ProcessEvent(ctx context.Context, m PubSubMessage) error {
-	d := unmarshalMessage(m)
-
-	if d.RotationType == "serviceAccountKey" {
-		key, err := locksmith.CreateServiceAccountKey(ctx, d)
-		if err != nil {
-			log.Fatal(err)
-		}
-		locksmith.VaultKey(ctx, key, d)
-
-	}
-
-	if d.RotationType != "serviceAccountKey" {
-		log.Fatal("Only serviceAccountKeyRotations are currently supported.")
-	}
-
-	return nil
-}
-
-```
-
-## Example Directive
-```json
-{
-    "rotation_type": "serviceAccountKey",
-    "service_account_email": "myserviceacccount@myprojectid.iam.gserviceaccount.com",
-    "disable_secret_versions": true,
-    "disable_service_account_keys": false,
-    "project_id": "my-project-id",
-    "secret_name": "my-secret"
-}
-```
+NOTE: The Identity and Access Management (IAM) and Secrets Manager APIs must be enabled in the projects where service account keys and secrets will be used.
--- locksmith.go
+++ locksmith.go
@@ -1,46 +1,22 @@
 package locksmith
 
-/*
-
-Locksmith allows you to automatically manage the creation, vaulting, rotation, and disabling
-of GCP Service Account Keys. Locksmith also allows you to optionally disable secrets versions
-in GCP Secret Manager.
-
-Locksmith is intended to be used withing a background Cloud Function on GCP. The entire solution would
-consist of a Cloud Scheduler job that would be responsible for ensuring each Service Account in a given
-GCP project has its keys rotated on a regular schedule. The Cloud Function's service account will need
-to be granted the requisite roles to create and disable Service Account keys and Secret versions.
-
-
-Example Message:
-
-{
-    "rotation_type": "serviceAccountKey",
-    "service_account_email": "myserviceacccount@myprojectid.iam.gserviceaccount.com",
-    "disable_secret_versions": true,
-    "disable_service_account_keys": false,
-    "project_id": "my-project-id",
-    "secret_name": "my-secret"
-}
-
-In the example above, the Cloud Function will attempt to create a new service account key for
-the "myserviceacccount@myprojectid.iam.gserviceaccount.com" account; create a new version for
-the secret "my-secret" and disable all other secret versions. It will *not* disable the existing
-keys for the service account.
-
-*/
-
 import (
 	"context"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
 
 	"google.golang.org/api/iam/v1"
 	"google.golang.org/api/iterator"
 
-	secretmanager "cloud.google.com/go/secretmanager/apiv1beta1"
-	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1beta1"
+	iampb "cloud.google.com/go/iam/apiv1/iampb"
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	secretmanagerpb "cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 )
 
 // Directive is used to tell the 'locksmith' what operations to perform.
@@ -50,47 +26,43 @@ type Directive struct {
 	// for specifying whether a GCP service account key or
 	// API Key is the target. Support may be extended for all
 	// secrets/keys on GCP that developers need/want to rotate.
-	RotationType string `json:"rotation_type,omitempty"`
+	RotationType string `json:"rotationType,omitempty"`
 
-	// The email of the service account.
-	ServiceAccountEmail string `json:"service_account_email"`
+	// The service account email whose keys will be rotated
+	ServiceAccountEmail string `json:"serviceAccountEmail"`
+
+	// The application service account that needs access to the secret
+	ApplicationServiceAccount string `json:"applicationServiceAccount"`
 
 	// Option to disable the secret version. If true, all previous versions
 	// of the secret will be disabled.
-	DisableSecretVersions bool `json:"disable_secret_versions,omitempty"`
+	DisableSecretVersions bool `json:"disableSecretVersions,omitempty"`
 
 	// Option to disable the key. If true all previous serviceAccount
 	// keys will be disabled.
-	DisableServiceAccoutKeys bool `json:"disable_service_account_keys,omitempty"`
-
-	// The ID of the GCP Project. ex: my-new-prj-123
-	ProjectID string `json:"project_id"`
+	DisableServiceAccountKeys bool `json:"disableServiceAccountKeys,omitempty"`
 
 	// The name of the secret. ex: my-prod-secret
-	SecretName string `json:"secret_name"`
+	// If omitted, a new secret will be created, unless an
+	// existing secret can be found that is tied to the same service account.
+	SecretName string `json:"secretName,omitempty"`
 }
-
-// KeyFile is the Service Account Key.
-// It is the secret that will be vaulted
-// in Secret Manager.
-var KeyFile []byte
 
 // CreateServiceAccountKey creates a service account key, and if DisableServiceAccountKeys
 // is set to 'true' in the directive, it will disable all other service account keys for that service
 // account. It will return one of []byte or error. The []byte (the KeyFile) contains the key material
 // of the service account. This should be treated as a secret and should only ever be placed in secret manager.
-func CreateServiceAccountKey(ctx context.Context, msg Directive) ([]byte, error) {
+func CreateServiceAccountKey(ctx context.Context, serviceAccountEmail string, disableAction bool) ([]byte, error) {
 	log.Println("Starting the process to create service account key...")
 
 	iamService, err := iam.NewService(ctx)
 	if err != nil {
 		return nil, err
 	}
-
-	serviceAccount := fmt.Sprintf("projects/%v/serviceAccounts/%v", msg.ProjectID, msg.ServiceAccountEmail)
+	serviceAccount := fmt.Sprintf("projects/-/serviceAccounts/%v", serviceAccountEmail)
 
 	// This will disable the service account keys if the directive states to do so.
-	if msg.DisableServiceAccoutKeys {
+	if disableAction {
 		disableServiceAccountKeys(iamService, serviceAccount)
 	}
 
@@ -103,12 +75,12 @@ func CreateServiceAccountKey(ctx context.Context, msg Directive) ([]byte, error)
 
 	log.Printf("Created service account key: %v", serviceAccount)
 
-	KeyFile, err = base64.StdEncoding.DecodeString(key.PrivateKeyData)
+	keyFile, err := base64.StdEncoding.DecodeString(key.PrivateKeyData)
 	if err != nil {
 		return nil, err
 	}
 
-	return KeyFile, err
+	return keyFile, err
 }
 
 // Disable any existing keys for the service account.
@@ -121,11 +93,14 @@ func disableServiceAccountKeys(iamService *iam.Service, serviceAccount string) e
 	var disable iam.DisableServiceAccountKeyRequest
 
 	for _, v := range resp.Keys {
-		_, err = iamService.Projects.ServiceAccounts.Keys.Disable(v.Name, &disable).Do()
-		if err != nil {
-			return err
+		if !v.Disabled {
+			_, err = iamService.Projects.ServiceAccounts.Keys.Disable(v.Name, &disable).Do()
+			if err != nil {
+				return err
+			}
+			log.Printf("Disabled the key: %v", v.Name)
 		}
-		log.Printf("Disabled the key: %v", v.Name)
+
 	}
 	return err
 }
@@ -133,24 +108,19 @@ func disableServiceAccountKeys(iamService *iam.Service, serviceAccount string) e
 // Create a new secret version and vaults the given value in that version.
 // If DisableSecretVersions is set to 'true' in the Directive, all other
 // version of the secret will be disabled.
-func VaultKey(ctx context.Context, key []byte, d Directive) error {
+func VaultKey(ctx context.Context, sm *secretmanager.Client, key []byte, secretName string, disableSecretAction bool) error {
 	log.Println("Starting the key vaulting process...")
 
-	sm, err := secretmanager.NewClient(ctx)
-	if err != nil {
-		return err
+	if disableSecretAction {
+		disableSecretVersions(ctx, sm, secretName)
 	}
 
-	secret := fmt.Sprintf("projects/%v/secrets/%v", d.ProjectID, d.SecretName)
-
-	if d.DisableSecretVersions {
-		disableSecretVersions(ctx, sm, secret)
+	req := &secretmanagerpb.AddSecretVersionRequest{
+		Payload: &secretmanagerpb.SecretPayload{
+			Data: key,
+		},
+		Parent: secretName,
 	}
-	req := &secretmanagerpb.AddSecretVersionRequest{}
-	payload := &secretmanagerpb.SecretPayload{}
-	payload.Data = key
-	req.Parent = secret
-	req.Payload = payload
 
 	version, err := sm.AddSecretVersion(ctx, req)
 	if err != nil {
@@ -190,4 +160,152 @@ func disableSecretVersions(ctx context.Context, sm *secretmanager.Client, secret
 
 	}
 	return nil
+}
+
+// Creates a secret in the given projectID and labels it.
+func createSecret(ctx context.Context, sm *secretmanager.Client, projectID string, serviceAccountEmail string) (string, error) {
+
+	s := fmt.Sprintf("lsm-%v-%v", projectID, rand.Intn(100000))
+
+	labels := make(map[string]string)
+	labels["islsm"] = "true"
+	labels["serviceaccountemail"] = strings.ReplaceAll(strings.ReplaceAll(serviceAccountEmail, "@", "-"), ".", "-")
+	createSecretRequest := &secretmanagerpb.CreateSecretRequest{
+		Parent:   fmt.Sprintf("projects/%v", projectID),
+		SecretId: s,
+		Secret: &secretmanagerpb.Secret{
+			Labels: labels,
+			Replication: &secretmanagerpb.Replication{
+				Replication: &secretmanagerpb.Replication_Automatic_{
+					Automatic: &secretmanagerpb.Replication_Automatic{}}},
+		},
+	}
+
+	resp, err := sm.CreateSecret(ctx, createSecretRequest)
+
+	if err != nil {
+		return "", err
+	}
+
+	return resp.Name, nil
+}
+
+// Checks if a secret exists or not for the given service account by checking secret labels that may have the email
+// address of the service account. If a secret has a matching label, we don't need to create a new secret for the new
+// service account key.
+func checkForSecret(ctx context.Context, sm *secretmanager.Client, projectID string, serviceAccountEmail string) (string, bool, error) {
+
+	req := &secretmanagerpb.ListSecretsRequest{
+		Parent: fmt.Sprintf("projects/%v", projectID),
+		Filter: fmt.Sprintf("labels.islsm=true AND labels.serviceaccountemail=%v", strings.ReplaceAll(strings.ReplaceAll(serviceAccountEmail, "@", "-"), ".", "-")),
+	}
+	it := sm.ListSecrets(context.TODO(), req)
+	for {
+		resp, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return "", false, err
+		}
+
+		if resp.Name != "" {
+			return resp.Name, true, nil
+		}
+	}
+	return "", false, nil
+}
+
+// TODO: Implement a process that will allow for rotating all service account keys
+// by listing all projects, all SAs in those projects, and then queueing them to be rotated
+// by existing functions.
+
+// Authoritative grant to access the secret data. Overwrites any existing policy.
+func grantSecretAccessor(ctx context.Context, sm *secretmanager.Client, secretName string, applicationServiceAccount string) error {
+
+	req := &iampb.SetIamPolicyRequest{
+		Resource: secretName,
+		Policy: &iampb.Policy{
+			Bindings: []*iampb.Binding{
+				{
+					Members: []string{"serviceAccount:" + applicationServiceAccount},
+					Role:    "roles/secretmanager.secretAccessor",
+				},
+			},
+		},
+	}
+	pol, err := sm.SetIamPolicy(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("new policy was created for secret: %v", pol)
+	return nil
+}
+
+// Ensures required values are provided in the directive.
+func (d Directive) validateDirective() (bool, error) {
+
+	if d.ServiceAccountEmail == "" || d.ApplicationServiceAccount == "" {
+
+		return false, fmt.Errorf("both ServiceAccountEmail and ApplicationServiceAccount email must be provided.ServiceAccountEmail received was %v and ApplicationServiceAccount provided was %v", d.ServiceAccountEmail, d.ApplicationServiceAccount)
+	}
+
+	return true, nil
+}
+
+// Cloud Function entrypoint.
+func Handler(w http.ResponseWriter, r *http.Request) {
+	var d Directive
+	err := json.NewDecoder(r.Body).Decode(&d)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	sm, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	switch isValid, err := d.validateDirective(); isValid {
+	case true:
+
+		serviceAccountKey, err := CreateServiceAccountKey(ctx, d.ServiceAccountEmail, d.DisableServiceAccountKeys)
+		if err != nil {
+			log.Fatal(err)
+		}
+		projectID := os.Getenv("SecureStoreProjectID")
+
+		secretName, exists, err := checkForSecret(ctx, sm, projectID, d.ServiceAccountEmail)
+		if err != nil {
+			log.Fatal(err)
+		}
+		switch exists {
+		case true:
+			err = VaultKey(ctx, sm, serviceAccountKey, secretName, d.DisableSecretVersions)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = grantSecretAccessor(ctx, sm, secretName, d.ApplicationServiceAccount)
+			if err != nil {
+				log.Fatal(err)
+			}
+		default:
+			secretName, err := createSecret(ctx, sm, projectID, d.ServiceAccountEmail)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			formattedSecretName := fmt.Sprintf("projects/%v/secrets/%v", projectID, secretName)
+			VaultKey(ctx, sm, serviceAccountKey, formattedSecretName, false)
+			err = grantSecretAccessor(ctx, sm, secretName, d.ApplicationServiceAccount)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+		}
+
+	default:
+		log.Fatal(err)
+	}
+
 }
